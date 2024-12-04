68fc5db688b6bc3a9f8b69a795990e9f47b1ff7e
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-12-05 01:06:50 +0200

    fix update post

--- controllers/postController.js
+++ controllers/postController.js
@@ -140,7 +140,7 @@ export const updatePost = async (req, res) => {
         title: req.body.title,
         text: req.body.text,
         imageUrl: req.body.imageUrl,
-        tags: req.body.tags,
+        tags: req.body.tags.split(","),
         user: req.userId,
       },
       { new: true }
